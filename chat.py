2b48430d998a623484ffe7eb9b4cf63db71493c7
rr8807837-hub <rr8807837@gmail.com>
2026-05-03 22:59:29 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on("send_message")
+def handle_send_message(data):
+    # send to ALL except sender
+    emit("receive_message", data, broadcast=True, include_self=False)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
